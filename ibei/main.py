5c7400a2e70e5d9ee7f8a73e43abbf0f7b992152
Joshua Ryan Smith <joshua.r.smith@gmail.com>
2014-09-13 18:43:21 -0400

    Add first draft of upper incomplete Bose-Einstein integral

---
+++ ibei/main.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+from astropy import constants
+from astropy import units
+from sympy.mpmath import polylog
+
+
+def uibei(order, energy_lo, temp, chem_potential):
+    """
+    Upper incomplete Bose-Einstein integral.
+    """
+    kT = temp * constants.k_B
+
+    reduced_energy_lo = energy_lo / kT
+    reduced_chem_potential = chem_potential / kT
+
+    prefactor = (2 * np.pi * np.math.factorial(order) * kT**(order + 1)) / \
+        (constants.h**3 * constants.c**2)
+
+    summand = 0
+
+    for indx in range(1, order + 2):
+        expt = (reduced_chem_potential - reduced_energy_lo).decompose()
+        term = reduced_energy_lo**(order - indx + 1) * polylog(indx, np.exp(expt)) / np.math.factorial(order - indx + 1)
+        summand += term
+
+    return summand
