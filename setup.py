d923cc7ecb6f8318d52d8fc38b1aba9c16cf40f6
Joshua Ryan Smith <joshua.r.smith@gmail.com>
2014-09-13 17:59:44 -0400

    Add distutils setup file

---
+++ setup.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+setup(name = "ibei",
+      version = "0.0.1",
+      author = "Joshua Ryan Smith",
+      author_email = "joshua.r.smith@gmail.com",
+      packages = ["ibei"],
+      url = "https://github.com/jrsmith3/ibei",
+      description = "Calculator for incomplete Bose-Einstein integral",
+      classifiers = ["Programming Language :: Python",
+                     "License :: OSI Approved :: MIT License",
+                     "Operating System :: OS Independent",
+                     "Development Status :: 3 - Alpha",
+                     "Intended Audience :: Science/Research",
+                     "Topic :: Scientific/Engineering :: Physics",
+                     "Natural Language :: English",],
+      install_requires = [],)
