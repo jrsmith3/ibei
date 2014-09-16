Fix uibei to return consistent units
The method uibei has two cases depending on the relative values of
energy_lo and chem_potential. Previously, this method would just
return a float if energy_lo < chem_potential, but now it returns a
value of zero with the correct units in that case.

Closes #3.

ibei/main.py
@@ -42,11 +42,11 @@ def uibei(order, energy_lo, temp, chem_potential):
     reduced_energy_lo = energy_lo / kT
     reduced_chem_potential = chem_potential / kT
 
-    if reduced_chem_potential > reduced_energy_lo:
-        return 0.
-
     prefactor = (2 * np.pi * np.math.factorial(order) * kT**(order + 1)) / \
         (constants.h**3 * constants.c**2)
+
+    if reduced_chem_potential > reduced_energy_lo:
+        return 0 * prefactor
 
     summand = 0
     expt = (reduced_chem_potential - reduced_energy_lo).decompose()