8882c77e6b96df7548357bce0a1c995fa2600e39
Joshua Ryan Smith <joshua.r.smith@gmail.com>
2014-09-16 10:35:56 -0400

    Add tests for issues causing exceptions

---
+++ test/test_iebi.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+import ibei
+from astropy import units
+import unittest
+
+temp_sun = 5762.
+temp_earth = 288.
+bandgap = 1.15
+
+class Issues(unittest.TestCase):
+    """
+    Tests output types of the calculator methods.
+    """
+    def test_issue_1_devos_efficiency(self):
+        """
+        Unit system needs to be specified for astropy.constants.e to work.
+        """
+        try:
+            ibei.devos_efficiency(bandgap, temp_sun, temp_earth, 1.09)
+        except:
+            self.fail("Unit system not initialized.")
