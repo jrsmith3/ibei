9cfc02596af6b5fe33f93fb17141a4a4baa85db9
Joshua Ryan Smith <joshua.r.smith@gmail.com>
2014-11-27 20:20:55 -0500

    Import all appropriate functionality

--- physicalproperty/__init__.py
+++ physicalproperty/__init__.py
@@ -6,6 +6,6 @@ Base Library (:mod:`physicalproperty`)
 .. currentmodule:: physicalproperty
 """
 
-from physicalproperty import PhysicalProperty
+from physicalproperty import PhysicalProperty, find_PhysicalProperty
 
 __version__ = "0.0.1"
