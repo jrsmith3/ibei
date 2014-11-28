40dd0738490a8fe27067b19e0539533a55c3b71c
Joshua Ryan Smith <joshua.r.smith@gmail.com>
2014-11-27 20:12:34 -0500

    Add init for multi-file module

---
+++ physicalproperty/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Base Library (:mod:`physicalproperty`)
+======================================
+
+.. currentmodule:: physicalproperty
+"""
+
+from physicalproperty import PhysicalProperty
+
+__version__ = "0.0.1"
