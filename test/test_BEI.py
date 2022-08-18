de73d488febcf03f9ebb8e5b8bd0df4aada9b6ed
Joshua Ryan Smith <joshua.r.smith@gmail.com>
2022-08-17 21:36:11 -0400

    Add test

--- test/test_BEI.py
+++ test/test_BEI.py
@@ -349,6 +349,22 @@ def test_consistency_upper_and_full_methods(valid_constructor_quantity_args):
     assert astropy.units.allclose(bei.upper(), bei.full())
 
 
+@pytest.mark.parametrize("order,helper_method_name",[
+            (2, "photon_flux"),
+            (3, "radiant_power_flux"),
+        ]
+    )
+def test_consistency_full_and_helper_methods(valid_constructor_quantity_args, order, helper_method_name):
+    """
+    `BEI.full` should equal `BEI.photon_flux` and `BEI.radiant_power_flux` for order 2 and 3, respectively.
+    """
+    valid_constructor_quantity_args["order"] = order
+    bei = ibei.models.BEI(**valid_constructor_quantity_args)
+    output = getattr(bei, helper_method_name)()
+
+    assert astropy.units.allclose(bei.upper(), output)
+
+
 # Pytest fixture definitions
 # ==========================
 @pytest.fixture
