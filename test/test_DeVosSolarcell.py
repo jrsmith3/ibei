d8ebafac46ad47545811628a37aceea5e1e91d6b
Joshua Ryan Smith <joshua.r.smith@gmail.com>
2022-08-22 20:39:45 -0400

    Add fixture

--- test/test_DeVosSolarcell.py
+++ test/test_DeVosSolarcell.py
@@ -131,12 +131,19 @@ class Issues():
 # Pytest fixture definitions
 # ==========================
 @pytest.fixture
-def valid_constructor_args():
+def valid_constructor_quantity_args():
     args = {
-        "solar_temperature": 5762.,
-        "planetary_temperature": 288.,
-        "bandgap": 1.15,
-        "voltage": 0.5,
+        "solar_temperature": astropy.units.Quantity(5762., astropy.units.K),
+        "planetary_temperature": astropy.units.Quantity(288., astropy.units.K),
+        "bandgap": astropy.units.Quantity(1.15, astropy.units.eV),
+        "voltage": astropy.units.Quantity(0.1, astropy.units.V),
         }
+
+    return args
+
+
+@pytest.fixture(params=[(lambda x: x), (lambda x: getattr(x, "value", x))])
+def valid_constructor_args(request, valid_constructor_quantity_args):
+    args = {key: request.param(val) for key, val in valid_constructor_quantity_args.items()}
 
     return args
