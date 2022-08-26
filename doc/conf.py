1e2cdac5cba56da7f112b05b703accbf8d9fd533
Joshua Ryan Smith <joshua.r.smith@gmail.com>
2022-08-26 15:26:08 -0400

    Remove superfluous config

--- doc/conf.py
+++ doc/conf.py
@@ -59,16 +59,9 @@ exclude_patterns = []
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#
+
 html_theme = 'sphinx_rtd_theme'
 
-html_sidebars = {
-    '**': [
-        'about.html',
-        'navigation.html',
-        'searchbox.html',
-    ]
-}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
